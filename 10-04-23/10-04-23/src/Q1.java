3efc768a730ba58d08b5d0a630775121342e9278
Aniket Krish Das <21052226@kiit.ac.in>
2023-04-20 15:30:55 +0530

    Fixed Bugs

--- 10-04-23/10-04-23/src/Q1.java
+++ 10-04-23/10-04-23/src/Q1.java
@@ -6,7 +6,7 @@ public class Q1 {
 
         // Find length of string
         int stringLength = string.length();
-        System.out.println("String length: " + string);
+        System.out.println("String length: " + stringLength);
 
         // Display content of string character by character
         for (int i = 0; i < stringLength; i++) {
@@ -14,19 +14,19 @@ public class Q1 {
         }
 
         // Convert to uppercase or vice versa
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < stringLength; i++) {
             if (Character.isLowerCase(string.charAt(i))) {
-                Character.toUpperCase(string.charAt(i));
+                sb.append(Character.toUpperCase(string.charAt(i)));
             }
 
             else {
-                Character.toLowerCase(string.charAt(i));
+                sb.append(Character.toLowerCase(string.charAt(i)));
             }
         }
 
         // Displaying newly-cased string
-        for (int i = 0; i < stringLength; i++) {
-            System.out.println(string.charAt(i) + " ");
-        }
+        System.out.println(sb);
     }
 }
