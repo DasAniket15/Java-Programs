076ab4c1783ed925c60831cdea9a9f2124392bf2
Aniket Krish Das <21052226@kiit.ac.in>
2023-05-07 15:33:45 +0530

    Updated Assignments

---
+++ Package Lab Assignment/src/MyPack/Number.java
@@ -0,0 +1,23 @@
+package MyPack;
+
+public class Number {
+    int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int find_factorial() {
+        int fact = 1;
+
+        for (int i = 1; i <= value; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+}
